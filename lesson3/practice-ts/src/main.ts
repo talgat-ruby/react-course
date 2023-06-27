3092109c04832f723fae2348e5bde566e6b74f50
Talgat <talgat.s@protonmail.com>
2023-06-27 20:29:01 +0600

    update lesson 3

--- lesson3/practice-ts/src/main.ts
+++ lesson3/practice-ts/src/main.ts
@@ -1,7 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.ts";
+
+function hello() {}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
